4e72f5a827608695d96e95e6185cb1d91ba9f809
Dr. Sanghita <sanghitabasu50@gmail.com>
2026-03-26 15:20:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+rain_dir = r"D:\D\Ruvision\IMD 1991_2024\IMD 1991_2017"
+boundary_fp = r"D:\C drive_21-06-2025\Downloads\Downloads_18-02-2026\Darjiling_Soreng Area Boundary Shapefile\Darjiling_Soreng Shapfiles.shp"
+landslide_fp = r"D:\C drive_21-06-2025\Downloads\Downloads_18-02-2026\landslide_point_78A04\landslide_point_78A04.shp"
+
+# CROPPING CONDITIONS
+ymin = 26.95
+xmax = 88.35
+
